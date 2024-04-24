90202134344e3a7e9ca96ca71e447e3cf5d18ab1
Mike Lubinets <git@mkl.dev>
2024-04-24 18:51:33 +0400

    move homebrew into a separate file

---
+++ darwin/homebrew.nix
@@ -0,0 +1,68 @@
+{
+  enable = true;
+
+  onActivation = {
+    autoUpdate = true;
+    upgrade = true;
+    cleanup = "zap";
+  };
+
+  taps = [
+    "homebrew/cask-fonts"
+  ];
+
+  brews = [
+    "mas"
+    "rclone"
+    "htop"
+    "minio-mc"
+  ];
+
+  caskArgs = {
+    # Install apps in ~/Applications
+    # Needed for Firefox Policies patching
+    appdir = "~/Applications";
+  };
+
+  casks = [
+    # System Utilities
+    "karabiner-elements"
+    "hammerspoon"
+    "raycast"
+    "wezterm"
+
+    # Development
+    "zed"
+    "visual-studio-code"
+
+    # Productivity
+    "firefox"
+    "obsidian"
+
+    # Communications
+    "telegram"
+
+    # Photo & Video Editing
+    "affinity-photo"
+    "affinity-designer"
+    "affinity-publisher"
+    "gyroflow"
+
+    # Fonts
+    "font-fira-code"
+    "font-jetbrains-mono"
+
+
+# TODO: investigate those programs:
+#      "soundsource"
+#      "bartender"
+#      "fantastical"
+  ];
+
+  masApps = {
+    "Final Cut Pro" = 424389933;
+    "Infuse" = 1136220934;
+    "Bitwarden" = 1352778147;
+    "Microsoft Word" = 462054704;
+  };
+}
